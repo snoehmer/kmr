0e50ebfb87e17e49563ae5f2efd9e3217d6ced0b
Robert Viehauser <robert.viehauser@gmail.com>
2012-11-29 21:29:38 +0100

    schaut jo moi ned so schlecht aus

--- src/chucknorris.cpp
+++ src/chucknorris.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
     Controller* controller = new Controller();
     HoleDetector* hd_ptr = controller->getHoleDetector();
 
-    ros::Subscriber asus = n.subscribe("/depth/image_raw", 1, &HoleDetector::asusCallback, hd_ptr);
+    ros::Subscriber asus = n.subscribe("/camera/depth/image_rect_raw", 1, &HoleDetector::asusCallback, hd_ptr);
 
 
     std::cout << "I'll find you!!!" << std::endl;
--- src/holedetector.cpp
+++ src/holedetector.cpp
@@ -9,11 +9,22 @@ HoleDetector::HoleDetector()
 
 void HoleDetector::asusCallback(const sensor_msgs::Image::ConstPtr& msg)
 {
-    std::cout << "ASUS-Callback!!!" << std::endl;
+    std::cout << "ASUS-Callback!!! Image encoding: " << msg->encoding << std::endl;
     int rows = msg->height;
     int cols = msg->width;
     signed char* data = (signed char*)&(msg->data.front());
+
     ROS_INFO("This is HoleDetector (asusCallback). Saving Image data... rows: [%d] cols: [%d]", rows, cols);
-    cv::Mat image = cv::Mat(rows, cols, CV_8SC1, data);
-    cv::imwrite("depth.png", image);
+
+    // value encoded in mm! 1.5m = 1500mm
+    cv::Mat image = cv::Mat(rows, cols, CV_16UC1, data);
+    cv::Mat temp;
+    image.convertTo(temp, CV_32FC1);
+    cv::threshold(temp, temp, 1500.0, 1500.0, cv::THRESH_TRUNC);
+    //cv::threshold(temp, temp, 1500.0, 1500.0, cv::THRESH_TOZERO_INV);
+    cv::normalize(temp, image, 0, 255, CV_MINMAX);
+
+
+    cv::imwrite("depth.jpg", image);
+    std::cout << "image written" << std::endl;
 }
