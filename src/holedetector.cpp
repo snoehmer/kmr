ceb4465221f608a99c0baa87cab971d622f3ab90
Robert Viehauser <robert.viehauser@gmail.com>
2012-12-04 11:52:30 +0100

    holy shit

--- src/holedetector.cpp
+++ src/holedetector.cpp
@@ -26,25 +26,25 @@ HoleDetector::HoleDetector()
 
 void HoleDetector::asusCallback(const sensor_msgs::Image::ConstPtr& msg)
 {
-    std::cout << "ASUS-Callback!!! Image encoding: " << msg->encoding << std::endl;
-    int rows = msg->height;
-    int cols = msg->width;
+    //std::cout << "ASUS-Callback!!! Image encoding: " << msg->encoding << std::endl;
+    int height = msg->height;
+    int width = msg->width;
     signed char* data = (signed char*)&(msg->data.front());
 
-    ROS_INFO("This is HoleDetector (asusCallback). Saving Image data... rows: [%d] cols: [%d]", rows, cols);
+    //ROS_INFO("This is HoleDetector (asusCallback). Saving Image data... rows: [%d] cols: [%d]", height, width);
 
     // value encoded in mm! 1.5m = 1500mm
-    cv::Mat image = cv::Mat(rows, cols, CV_16UC1, data);
+    cv::Mat image = cv::Mat(height, width, CV_16UC1, data);
     cv::Mat temp;
     image.convertTo(temp, CV_32FC1);
-    cv::threshold(temp, temp, 1500.0, 1500.0, cv::THRESH_TRUNC);
+    cv::threshold(temp, temp, 1500.0, 1500.0, cv::THRESH_TRUNC); // truncate depth to 1,5m
     //cv::threshold(temp, temp, 1500.0, 1500.0, cv::THRESH_TOZERO_INV);
     cv::normalize(temp, image, 0, 255, CV_MINMAX);
 
 
     GaussianBlur( image, image, Size(3,3), 0, 0, BORDER_DEFAULT );
     cv::imwrite("depth.jpg", image);
-    std::cout << "image written" << std::endl;
+    //std::cout << "image written" << std::endl;
 
 
 
@@ -97,18 +97,31 @@ void HoleDetector::asusCallback(const sensor_msgs::Image::ConstPtr& msg)
     Mat mask;
     mask = image_8bit >= 0x50;
 
-    // int _delta, int _min_area, int _max_area, float _max_variation, float _min_diversity, int _max_evolution, double _area_threshold, double _min_margin, int _edge_blur_size
-    MSER extractor(5, 5000, 50000, 0.1f, 0.1f, 256, 0.1, 40.0f, 3);
+    //             int _delta, int _min_area,     int _max_area,   float _max_variation, float _min_diversity, int _max_evolution, double _area_threshold, double _min_margin, int _edge_blur_size
+    MSER extractor(5,          width*height/1000, width*height/10, 0.1f,                 0.1f,                 256,                0.1,                    40.0f,              3);
     extractor(image_8bit, msers, mask);
 
     cvtColor(image_8bit, contour_image, CV_GRAY2BGR);
 
+    double area;
 
     std::cout << "MSERS: " << msers.size() << std::endl;
-    double area;
     for (size_t index = 0; index < msers.size(); index ++)
     {
-          drawContours(contour_image, msers, index, colours[index%9], 1);
+        Rect rect = boundingRect(msers[index]);
+        if((float)rect.width/rect.height < 2.5f)
+        {
+           drawContours(contour_image, msers, index, colours[index%9], 1);
+
+           Point center = Point(rect.x+rect.width/2, rect.y+rect.height/2);
+           line(contour_image, center + Point(0, rect.height/2), center - Point(0, rect.height/2), Scalar(0,0,0), 3);
+           line(contour_image, center + Point(rect.width/2, 0), center - Point(rect.width/2, 0), Scalar(0,0,0), 3);
+        }
+        rectangle(contour_image, rect, colours[index%9]);
+
+        area = contourArea(msers[index]);
+        std::cout << "stats: [" << index << "] area: " << area << ",  h/w: " << (float)rect.width/rect.height << std::endl;
+
     }
 
 
